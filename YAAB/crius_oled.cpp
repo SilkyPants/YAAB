37c018fd4767b97a595c293c422952f6ab008db5
Dan Silk <dfsilk@gmail.com>
2015-05-05 11:51:30 +0930

    Updated Visual Studio project files to work with the latest SDKs and plugins
    Arduino 1.6.3
    Visual Micro (pre release)
    Visual Studio Community 2013

--- YAAB/crius_oled.cpp
+++ YAAB/crius_oled.cpp
@@ -19,7 +19,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <string.h> // for memset
 #include <stdlib.h>
 #include <avr/pgmspace.h>
 
